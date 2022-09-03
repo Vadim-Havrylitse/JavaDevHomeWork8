dd1346500b9276381d0dd22d488b2b10eeed50cd
Vadim-Havrylitse <vfgavrilice@gmail.com>
2022-09-03 18:06:47 +0300

    Finish action

--- src/main/java/com/example/JavaDevHomeWork8/JavaDevHomeWork8Application.java
+++ src/main/java/com/example/JavaDevHomeWork8/JavaDevHomeWork8Application.java
@@ -1,15 +1,10 @@
 package com.example.JavaDevHomeWork8;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -18,13 +13,6 @@ public class JavaDevHomeWork8Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDevHomeWork8Application.class, args);
 	}
-
-//	@Bean
-//	@Lazy
-//	public AWSCredentials amazonAWSCredentials (@Value("${aws.access.key}") String awsKey,
-//												@Value("${aws.access.secret.key}") String awsSecretKey){
-//		return new BasicAWSCredentials(awsKey, awsSecretKey);
-//	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
--- src/main/java/com/example/JavaDevHomeWork8/maincontroller/MainController.java
+++ src/main/java/com/example/JavaDevHomeWork8/maincontroller/MainController.java
@@ -2,9 +2,8 @@ package com.example.JavaDevHomeWork8.maincontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
