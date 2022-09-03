c4d2de044fec679fe2ce41154a6b4acb17fe21fb
Vadim-Havrylitse <vfgavrilice@gmail.com>
2022-09-03 13:44:07 +0300

    Init heroku git

--- src/main/java/com/example/JavaDevHomeWork8/JavaDevHomeWork8Application.java
+++ src/main/java/com/example/JavaDevHomeWork8/JavaDevHomeWork8Application.java
@@ -1,27 +1,34 @@
 package com.example.JavaDevHomeWork8;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import lombok.RequiredArgsConstructor;
-import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class JavaDevHomeWork8Application {
-
-	private final DataSource dataSource;
+public class JavaDevHomeWork8Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDevHomeWork8Application.class, args);
 	}
 
 //	@Bean
-//	public void flywayMigrate(){
-//		System.err.println("MIGRATION!!!");
-//		System.out.println("dataSource = " + dataSource.toString());
-//		Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().repair();
+//	@Lazy
+//	public AWSCredentials amazonAWSCredentials (@Value("${aws.access.key}") String awsKey,
+//												@Value("${aws.access.secret.key}") String awsSecretKey){
+//		return new BasicAWSCredentials(awsKey, awsSecretKey);
 //	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaDevHomeWork8Application.class);
+	}
+
 }
--- src/main/java/com/example/JavaDevHomeWork8/maincontroller/MainController.java
+++ src/main/java/com/example/JavaDevHomeWork8/maincontroller/MainController.java
@@ -1,25 +1,13 @@
 package com.example.JavaDevHomeWork8.maincontroller;
 
-import com.example.JavaDevHomeWork8.user.dao.UserService;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequiredArgsConstructor
 public class MainController {
-    private final UserService userService;
 
-    @SneakyThrows
     @GetMapping("/")
-    public String getMainPage(@RequestParam(name = "p1", required = false, defaultValue = "default") String param1,
-                              Model model){
-            StringBuilder str = new StringBuilder();
-            userService.getAll().forEach(x -> str.append(x.toString()).append("\n"));
-            model.addAttribute("label", str.toString());
+    public String getMainPage(){
         return "main-page";
     }
 }
